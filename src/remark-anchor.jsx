ee70ad04cfbec7aaf7b36d0c72106e9f2d926f66
elpnt <quantumular@gmail.com>
2021-08-27 16:10:29 +0900

    link card for standard URLs

--- src/remark-anchor.jsx
+++ src/remark-anchor.jsx
@@ -1,32 +1,4 @@
 import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
-import ogs from "open-graph-scraper";
-
-function fetchTitleAndImage(url) {
-  let title = null;
-  let image = null;
-
-  fetch(url)
-    .then((res) => res.text())
-    .then((text) => {
-      const dom = new DOMParser().parseFromString(text, "text/html");
-
-      // Extract title
-      // If title doesn't exit, use URL instead
-      const headTitle = dom.head.getElementsByTagName("title")[0].text;
-      let ogTitle = dom.head.querySelector("meta[property='og:title']");
-      ogTitle = ogTitle ? ogTitle.getAttribute("content") : headTitle;
-      title = ogTitle || href;
-
-      // Extract image
-      const ogImage = dom.head.querySelector("meta[property='og:image']");
-      image = ogImage ? ogImage.getAttribute("content").split("?")[0] : null;
-    });
-
-  console.log;
-
-  return { title, image };
-}
 
 export default function createRemarkAnchor(OrigA) {
   return function RemarkAnchor(props) {
@@ -36,36 +8,60 @@ export default function createRemarkAnchor(OrigA) {
 
     let [title, setTitle] = useState(null);
     let [image, setImage] = useState(null);
+    let [description, setDescription] = useState(null);
 
     useEffect(() => {
-      fetch(href)
-        .then((res) => res.text())
-        .then((text) => {
-          const dom = new DOMParser().parseFromString(text, "text/html");
+      // To avoid too many requests while editing, sleep 1 second for each request
+      const timer = setTimeout(() => {
+        fetch(href)
+          .then((res) => res.text())
+          .then((text) => {
+            const dom = new DOMParser().parseFromString(text, "text/html");
 
-          // Extract title
-          // If title doesn't exit, use URL instead
-          const headTitle = dom.head.getElementsByTagName("title")[0].text;
-          let ogTitle = dom.head.querySelector("meta[property='og:title']");
-          ogTitle = ogTitle ? ogTitle.getAttribute("content") : headTitle;
-          title = ogTitle || href;
-          setTitle(title);
+            // Extract title
+            // If title doesn't exit, use URL instead
+            const headTitle = dom.head.getElementsByTagName("title")[0].text;
+            let ogTitle = dom.head.querySelector("meta[property='og:title']");
+            ogTitle = ogTitle ? ogTitle.getAttribute("content") : headTitle;
+            setTitle(ogTitle || href);
 
-          // Extract image
-          const ogImage = dom.head.querySelector("meta[property='og:image']");
-          image = ogImage
-            ? ogImage.getAttribute("content").split("?")[0]
-            : null;
-          setImage(image);
-        });
+            // Extract description
+            let ogDescription = dom.head.querySelector(
+              "meta[property='og:description']"
+            );
+            ogDescription = ogDescription
+              ? ogDescription.getAttribute("content")
+              : null;
+            setDescription(ogDescription);
+
+            // Extract image
+            const ogImage = dom.head.querySelector("meta[property='og:image']");
+            image = ogImage
+              ? ogImage.getAttribute("content").split("?")[0]
+              : null;
+            // If og:image is relative path, convert it to absolute one
+            if (image && !isAbsoluteUrl(image)) {
+              const origin = new URL(href).origin;
+              image = origin + "/" + image;
+            }
+            setImage(image);
+          });
+      }, 1000);
+
+      return () => clearTimeout(timer);
     });
 
-    if (isAutoLinkEnabled && label === href) {
+    if (isAutoLinkEnabled && label === href && image) {
       return (
-        <div className="link-card">
-          <img src={image} className="link-card-image" />
-          <span className="link-card-title">{title}</span>
-        </div>
+        <a href={href}>
+          <div className="link-card">
+            <img src={image} className="link-card-image" />
+            <div className="link-card-text">
+              <span className="link-card-title">{title}</span>
+              <span className="link-card-description">{description}</span>
+            </div>
+          </div>
+        </a>
       );
     }
 
@@ -75,4 +71,8 @@ export default function createRemarkAnchor(OrigA) {
       return <a {...props}>{children}</a>;
     }
   };
+}
+
+function isAbsoluteUrl(url) {
+  return url.indexOf("://") > 0 || url.indexOf("//") > 0;
 }
