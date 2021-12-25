7d3d386969b32bc47ab69e4c6ff567379150e08d
elpnt <quantumular@gmail.com>
2021-12-25 14:19:52 +0900

    use `open-graph-scraper`

--- src/useFetchData.js
+++ src/useFetchData.js
@@ -1,73 +1,45 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from 'react';
+import ogs from 'open-graph-scraper'
 
-function fetchImage(url, dom) {
-  let image = "";
-  const ogImage = dom.querySelector("meta[property='og:image']");
-  if (ogImage) image = ogImage.getAttribute("content").split("?")[0];
-  const twitterImage = dom.querySelector("meta[name='twitter:image']");
-  if (!image && twitterImage) {
-    image = twitterImage.getAttribute("content").split("?")[0];
-  }
-
-  // If the image path is relative, convert it to absolute one
-  const isAbsoluteUrl = (url) =>
-    url.indexOf("://") > 0 || url.indexOf("//") > 0;
-  if (image && !isAbsoluteUrl(image)) {
-    const origin = new URL(url).origin;
-    if (url[0] === ".") {
-      // relative to the current URL
-      image = url + "/" + image;
-    } else {
-      // relative to the origin
-      image = origin + "/" + image;
-    }
-  }
-
-  return image;
-}
-
-function fetchTitle(dom) {
-  let title = "";
-  const headTitle = dom.querySelector("title");
-  if (headTitle) title = headTitle.text;
-  const ogTitle = dom.querySelector("meta[property='og:title']");
-  if (!title && ogTitle) title = ogTitle.getAttribute("content");
-  const twitterTitle = dom.querySelector("meta[name='twitter:title']");
-  if (!title && twitterTitle) title = twitterTitle.getAttribute("content");
-
-  return title;
-}
-
-function fetchDescription(dom) {
-  let description = "";
-  const metaDescription = dom.querySelector("meta[property='description']");
-  if (metaDescription) description = metaDescription.getAttribute("content");
-  const ogDescription = dom.querySelector("meta[property='og:description']");
-  if (ogDescription) description = ogDescription.getAttribute("content");
-
-  return description;
-}
+const SLEEP_AFTER_FETCH = 1000;
 
 export function useFetchData(href) {
   const [image, setImage] = useState("");
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
 
-  // To avoid too many requests, sleep 1 second after editing
   useEffect(() => {
     const timer = setTimeout(() => {
       fetch(href)
-        .then((response) => response.text())
-        .then((text) => {
-          const dom = new DOMParser().parseFromString(text, "text/html");
-          setImage(fetchImage(href, dom));
-          setTitle(fetchTitle(dom));
-          setDescription(fetchDescription(dom) || href);
-        });
-    }, 1000);
+        .then(response => response.text())
+        .then(html => ogs({ html }))
+        .then(({ result }) => {
+          const {
+            ogImage,
+            ogTitle,
+            ogDescription,
+            twitterImage,
+            twitterTitle,
+            twitterDescription
+          } = result;
+
+          setTitle(ogTitle || twitterTitle);
+          setDescription(ogDescription || twitterDescription)
+          return ogImage?.url || twitterImage?.url;
+        })
+        .then(rawImageUrl => {
+          const imageUrl = new URL(rawImageUrl, href).href; // Absorb the difference between absolute and relative URLs
+          return fetch(imageUrl)
+        })
+        .then(({ ok, url }) => {
+          if (ok) {
+            setImage(url)
+          }
+        })
+    }, SLEEP_AFTER_FETCH)
 
     return () => clearTimeout(timer);
-  }, [href]);
+  }, [href])
 
   return {
     image,
